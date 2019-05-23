8b809108a90c6dca843442aa8555c8424fd48f30
ShirleyC7 <okchengzi@163.com>
2019-05-23 22:29:19 +0800

    加入键盘 背景 日志

--- src/GUI/Form1.Designer.cs
+++ src/GUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tb_keyboard = new System.Windows.Forms.TextBox();
@@ -36,63 +37,61 @@
             this.label3 = new System.Windows.Forms.Label();
             this.tb_url = new System.Windows.Forms.TextBox();
             this.btn_start = new System.Windows.Forms.Button();
-            this.btn_save = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.btn_stop = new System.Windows.Forms.Button();
             this.btn_read = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 26);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(5, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.Size = new System.Drawing.Size(40, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "键盘";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 52);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(5, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.Size = new System.Drawing.Size(40, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "鼠标";
             // 
             // tb_keyboard
             // 
-            this.tb_keyboard.Location = new System.Drawing.Point(70, 23);
-            this.tb_keyboard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_keyboard.Location = new System.Drawing.Point(93, 31);
             this.tb_keyboard.Name = "tb_keyboard";
             this.tb_keyboard.ReadOnly = true;
-            this.tb_keyboard.Size = new System.Drawing.Size(159, 21);
+            this.tb_keyboard.Size = new System.Drawing.Size(211, 26);
             this.tb_keyboard.TabIndex = 2;
             // 
             // tb_mouse
             // 
-            this.tb_mouse.Location = new System.Drawing.Point(70, 48);
-            this.tb_mouse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_mouse.Location = new System.Drawing.Point(93, 64);
             this.tb_mouse.Name = "tb_mouse";
             this.tb_mouse.ReadOnly = true;
-            this.tb_mouse.Size = new System.Drawing.Size(159, 21);
+            this.tb_mouse.Size = new System.Drawing.Size(211, 26);
             this.tb_mouse.TabIndex = 3;
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(3)))));
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.tb_mouse);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.tb_keyboard);
-            this.groupBox1.Location = new System.Drawing.Point(44, 94);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(34, 88);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(243, 80);
+            this.groupBox1.Size = new System.Drawing.Size(324, 107);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "实时状态";
@@ -100,83 +99,128 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(44, 54);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(34, 35);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.Size = new System.Drawing.Size(64, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "目标url";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // tb_url
             // 
-            this.tb_url.Location = new System.Drawing.Point(107, 52);
-            this.tb_url.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_url.Location = new System.Drawing.Point(118, 32);
             this.tb_url.Name = "tb_url";
-            this.tb_url.Size = new System.Drawing.Size(381, 21);
+            this.tb_url.Size = new System.Drawing.Size(507, 26);
             this.tb_url.TabIndex = 6;
             this.tb_url.Text = "http://localhost:8086/announce";
             // 
             // btn_start
             // 
-            this.btn_start.Location = new System.Drawing.Point(342, 113);
-            this.btn_start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_start.BackColor = System.Drawing.Color.White;
+            this.btn_start.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_start.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_start.Location = new System.Drawing.Point(431, 111);
             this.btn_start.Name = "btn_start";
-            this.btn_start.Size = new System.Drawing.Size(56, 18);
+            this.btn_start.Size = new System.Drawing.Size(90, 28);
             this.btn_start.TabIndex = 7;
             this.btn_start.Text = "开始";
-            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.UseVisualStyleBackColor = false;
             this.btn_start.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btn_save
+            // saveFileDialog1
             // 
-            this.btn_save.Location = new System.Drawing.Point(431, 113);
-            this.btn_save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(56, 18);
-            this.btn_save.TabIndex = 8;
-            this.btn_save.Text = "保存记录";
-            this.btn_save.UseVisualStyleBackColor = true;
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
             // btn_stop
             // 
-            this.btn_stop.Location = new System.Drawing.Point(342, 146);
-            this.btn_stop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_stop.BackColor = System.Drawing.Color.White;
+            this.btn_stop.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_stop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_stop.Location = new System.Drawing.Point(431, 152);
             this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(56, 18);
+            this.btn_stop.Size = new System.Drawing.Size(90, 28);
             this.btn_stop.TabIndex = 9;
             this.btn_stop.Text = "停止";
-            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.UseVisualStyleBackColor = false;
             this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
             // 
             // btn_read
             // 
-            this.btn_read.Location = new System.Drawing.Point(431, 146);
-            this.btn_read.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_read.BackColor = System.Drawing.Color.White;
+            this.btn_read.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_read.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_read.Location = new System.Drawing.Point(588, 150);
             this.btn_read.Name = "btn_read";
-            this.btn_read.Size = new System.Drawing.Size(56, 18);
+            this.btn_read.Size = new System.Drawing.Size(111, 30);
             this.btn_read.TabIndex = 10;
-            this.btn_read.Text = "读取记录";
-            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Text = "读取日志";
+            this.btn_read.UseVisualStyleBackColor = false;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Consolas", 15.1875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(34, 226);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(405, 148);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(3)))));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 10.6875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(456, 226);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(313, 219);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(588, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 30);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "写入日志";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 222);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(781, 457);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.btn_read);
             this.Controls.Add(this.btn_stop);
-            this.Controls.Add(this.btn_save);
             this.Controls.Add(this.btn_start);
             this.Controls.Add(this.tb_url);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "键鼠操作监控程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -194,11 +238,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tb_url;
         private System.Windows.Forms.Button btn_start;
-        private System.Windows.Forms.Button btn_save;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button btn_stop;
         private System.Windows.Forms.Button btn_read;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
